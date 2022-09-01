bf1df726bcebcf072ad3f54d2eb5429367218526
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-01 22:30:33 +0100

    add data file

---
+++ src/asssets/data.js
@@ -0,0 +1,164 @@
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+export const aboutCard = [
+	{
+		id: 1,
+		icon: <FaAward className="about__icon" />,
+		title: "Experience",
+		info: "3+ Years Working",
+	},
+	{
+		id: 2,
+		icon: <FiUsers className="about__icon" />,
+		title: "Clients",
+		info: "11+ Worldwide",
+	},
+	{
+		id: 3,
+		icon: <VscFolderLibrary className="about__icon" />,
+		title: "Projects",
+		info: "20+ Completed",
+	},
+];
+
+export const frontendExperience = [
+	{
+		id: 1,
+		title: "HTML",
+		level: "Experienced",
+	},
+	{
+		id: 2,
+		title: "CSS",
+		level: "Intermediate",
+	},
+	{
+		id: 3,
+		title: "Bootstrap",
+		level: "Intermediate",
+	},
+	{
+		id: 4,
+		title: "Tailwind",
+		level: "Intermdiate",
+	},
+	{
+		id: 5,
+		title: "Javascript",
+		level: "Experienced",
+	},
+	{
+		id: 6,
+		title: "React",
+		level: "Experienced",
+	},
+];
+
+export const backendExperience = [
+	{
+		id: 1,
+		title: "Node JS",
+		level: "Experienced",
+	},
+	{
+		id: 2,
+		title: "MongoDB",
+		level: "Intermediate",
+	},
+	{
+		id: 3,
+		title: "Express",
+		level: "Intermediate",
+	},
+	{
+		id: 4,
+		title: "MySQL",
+		level: "Basic",
+	},
+	{
+		id: 5,
+		title: "Python",
+		level: "Basic",
+	},
+];
+
+export const dataScience = [
+	{
+		id: 1,
+		service: "Data Manipulation using Excel.",
+	},
+	{
+		id: 2,
+		service:
+			"Very strong in producing insights with beautiful data visualisations using Tableau",
+	},
+	{
+		id: 3,
+		service: "Proficient in analysing data with python and many libraries",
+	},
+	{
+		id: 4,
+		service:
+			"EDA e.g Explore data using  pandas, matplotlib, seaborn, plotly and manymore!",
+	},
+	{
+		id: 5,
+		service:
+			"Vast in creating ML Models Surpervised & Unsupervised using various Algo and deploying them (Streamlit).",
+	},
+];
+export const webDev = [
+	{
+		id: 1,
+		service: "Build responsive and beautiful pages.(HTML, CSS & JS).",
+	},
+	{
+		id: 2,
+		service: "Develope your game of interest using JavaScript.",
+	},
+	{
+		id: 3,
+		service:
+			"React best practice (Router, hooks, usestate, reusable component and many more) .",
+	},
+	{
+		id: 4,
+		service: "Build modern Fullstack project using MERN stack.!",
+	},
+	{
+		id: 5,
+		service:
+			"Deploy & Maintain your website using the best Domain & Hosting provider in the world",
+	},
+	{
+		id: 6,
+		service:
+			"Authentication and security website using Encrypted & secured database.",
+	},
+];
+export const tutorials = [
+	{
+		id: 1,
+		service:
+			"Learn the fundamantals of Programming Languages with various real life examples.",
+	},
+	{
+		id: 2,
+		service: "learn how to use Excel for data Validation & Manipulation.",
+	},
+	{
+		id: 3,
+		service: "Fundamentals of Structured programming using C Language.",
+	},
+	{
+		id: 4,
+		service:
+			"Learn the basics Python Programming Language and how they can be applied in today's world.",
+	},
+	{
+		id: 5,
+		service: "All you need to know to get started in the technology world!",
+	},
+];
